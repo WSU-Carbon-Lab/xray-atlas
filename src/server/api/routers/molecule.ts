8a5c9b28b4809b96f93bc0e95424b1da84e1516f
HarlanHeilman <Harlan.Heilman@wsu.edu>
2024-02-11 01:30:58 -0800

    Search - Home Page Remodel

--- src/server/api/routers/molecule.ts
+++ src/server/api/routers/molecule.ts
@@ -1,44 +1,77 @@
 import { clerkClient } from "@clerk/nextjs";
 import { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
 const fileterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    imageUrl: user.imageUrl,
-  };
+    return {
+        id: user.id,
+        username: user.username,
+        imageUrl: user.imageUrl,
+    };
 }
 
 export const moleculeRouter = createTRPCRouter({
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    const molecules = await ctx.db.molecularRecord.findMany({
-      orderBy: { createdAt: "desc" },
-      take: 100,
-    });
-
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: molecules.map((molecule) => molecule.authorId),
-        limit: 100,
-      })
-    ).map(fileterUserForClient);
-
-    return molecules.map((molecule) => {
-      const author = users.find((user) => user.id === molecule.authorId);
-
-      if (!author || !author.username)
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author not found",
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        const molecules = await ctx.db.molecularRecord.findMany({
+            orderBy: { createdAt: "desc" },
+            take: 100,
         });
 
-      return {
-        molecule,
-        author,
-      };
-    });
-  }),
+        const users = (
+            await clerkClient.users.getUserList({
+                userId: molecules.map((molecule) => molecule.authorId),
+                limit: 100,
+            })
+        ).map(fileterUserForClient);
+
+        return molecules.map((molecule) => {
+            const author = users.find((user) => user.id === molecule.authorId);
+
+            if (!author || !author.username)
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Author not found",
+                });
+
+            return {
+                molecule,
+                author,
+            };
+        });
+    }),
+
+    create: privateProcedure
+        .input(
+            z.object({
+                chemicalFormula: z.string().min(1).max(1000),
+                chemicalName: z.string().min(1).max(1000),
+                materialName: z.string().min(1).max(1000),
+                solvent: z.string().min(1).max(1000),
+                concentration: z.string().min(1).max(1000),
+                temperature: z.string().min(1).max(1000),
+                depositionTime: z.string().min(1).max(1000),
+                spinSpeed: z.string().min(1).max(1000),
+                vendorName: z.string().min(1).max(1000),
+
+                // Experimental Conditions
+                absorbtionEdge: z.string().min(1).max(1000),
+                lab: z.string().min(1).max(1000),
+                scanMode: z.string().min(1).max(1000),
+
+            })
+        ).mutation(async ({ ctx, input }) => {
+            const authorId = ctx.currentUser;
+
+            const molecule = await ctx.db.molecularRecord.create({
+                data: {
+                    ...input,
+                    authorId,
+                },
+            })
+
+            return molecule;
+        })
 });
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -6,7 +6,8 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
-import { initTRPC } from "@trpc/server";
+import { currentUser, getAuth } from "@clerk/nextjs/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import superjson from "superjson";
 import { ZodError } from "zod";
@@ -14,39 +15,22 @@ import { ZodError } from "zod";
 import { db } from "~/server/db";
 
 /**
- * 1. CONTEXT
- *
- * This section defines the "contexts" that are available in the backend API.
- *
- * These allow you to access things when processing a request, like the database, the session, etc.
- */
-
-type CreateContextOptions = Record<string, never>;
-
-/**
- * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
- * it from here.
- *
- * Examples of things you may need it for:
- * - testing, so we don't have to mock Next.js' req/res
- * - tRPC's `createSSGHelpers`, where we don't have req/res
- *
- * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
- */
-const createInnerTRPCContext = (_opts: CreateContextOptions) => {
-  return {
-    db,
-  };
-};
-
-/**
  * This is the actual context you will use in your router. It will be used to process every request
  * that goes through your tRPC endpoint.
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = (_opts: CreateNextContextOptions) => {
-  return createInnerTRPCContext({});
+export const createTRPCContext = (opts: CreateNextContextOptions) => {
+
+  const { req } = opts;
+  const sesh = getAuth(req);
+
+  const user = sesh.userId
+
+  return {
+    db,
+    currentUser: user,
+  }
 };
 
 /**
@@ -93,3 +77,19 @@ export const createTRPCRouter = t.router;
  * are logged in.
  */
 export const publicProcedure = t.procedure;
+
+const enforceUserIsAuth = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.currentUser) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED"
+    });
+  }
+
+  return next({
+    ctx: {
+      currentUser: ctx.currentUser,
+    }
+  });
+});
+
+export const privateProcedure = t.procedure.use(enforceUserIsAuth);
