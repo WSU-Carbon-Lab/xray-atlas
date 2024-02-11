7a63a990f932fc89dd000e04f27747b2c388a8d8
HarlanHeilman <Harlan.Heilman@wsu.edu>
2024-02-10 20:54:20 -0800

    next-image - style up

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    domains: ["images.clerk.dev", "img.clerk.com"],
+  },
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
--- src/server/api/routers/molecule.ts
+++ src/server/api/routers/molecule.ts
@@ -1,11 +1,45 @@
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const fileterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    imageUrl: user.imageUrl,
+  };
+}
+
 export const moleculeRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.molecularRecord.findMany({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const molecules = await ctx.db.molecularRecord.findMany({
       orderBy: { createdAt: "desc" },
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        authorId: molecules.map((molecule) => molecule.authorId),
+        limit: 100,
+      })
+    ).map(fileterUserForClient);
+
+    return molecules.map((molecule) => {
+      const author = users.find((user) => user.id === molecule.authorId);
+
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author not found",
+        });
+
+      return {
+        molecule,
+        author,
+      };
     });
   }),
 });
