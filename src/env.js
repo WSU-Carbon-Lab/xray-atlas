e4e8984f4eacb9ae4e5e36065ea7aa0744a9d226
Harlan <Harlan.Heilman@wsu.edu>
2025-09-03 16:24:11 -0700

    feat: initialize usertable api

--- src/env.js
+++ src/env.js
@@ -10,11 +10,19 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    CLERK_SECRET_KEY: z.string().min(1),
+    MOLECULES_TABLE_NAME: z.string().min(1).default("molecules"),
+    USERS_TABLE_NAME: z.string().min(1).default("users"),
   },
   client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    MOLECULES_TABLE_NAME: process.env.MOLECULES_TABLE_NAME,
+    USERS_TABLE_NAME: process.env.USERS_TABLE_NAME,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
