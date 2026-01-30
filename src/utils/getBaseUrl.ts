5ff35ac2a7077034c42ab57e139150f0b71f6754
Harlan D Heilman <73567020+HarlanHeilman@users.noreply.github.com>
2026-01-29 22:14:10 -0800

    rollback: added back vercel fallback just for saftey. not needed but just doing it

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,4 +1,6 @@
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
-  return process.env.AUTH_URL!;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.AUTH_URL) return process.env.AUTH_URL;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
 }
